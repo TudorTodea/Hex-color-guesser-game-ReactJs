3b7019e0eeef9890180d62fc6f1d23e45798fa55
TudorTodea <tudortodea1416@gmail.com>
2022-11-25 09:00:54 +0200

    Added base concept of the game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import { useEffect,useState } from 'react';
 import './App.css';
 
 function App() {
+
+const[color,setColor]=useState('');
+const [answers,setAnswers]=useState([])
+const [result,setResult]=useState(null)
+const [score,setScore]=useState(0);
+const [bestScore,setBestScore]=useState(0)
+const generateColor=()=>{
+  const digits=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+  
+  const color= new Array(6).fill(0).map((el)=> el=digits[Math.floor(Math.random()*digits.length)]).join('')
+
+return '#'+color
+}
+
+useEffect(()=>{
+const actualColor=generateColor();
+setColor(actualColor);
+setAnswers([actualColor,generateColor(),generateColor()].sort(()=>0.5 - Math.random()))
+
+},[])
+
+const answerHandler=(e)=>{
+if(e.target.outerText===color){
+const actualColor=generateColor();
+setColor(actualColor);
+setAnswers([actualColor,generateColor(),generateColor()].sort(()=>0.5 - Math.random()))
+setResult(true)
+setScore((prev)=>{
+  if(prev+1>bestScore)
+  setBestScore(prev+1);
+return prev+1
+})
+}else{
+setResult(false)
+setScore(0);
+}
+
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+      <div>Best Score: {bestScore}</div>
+      <div style={{ backgroundColor:color}}  className='block'>
+      </div>
+      </div>
+      <div className='answers-container'>
+       { answers.map((answer)=> <button onClick={(e)=>answerHandler(e)} className='answers' key={answer}>{answer}</button>)}
+          
+      </div>
+      {result===true&&
+     <div className='result-corect'>
+    Corect!
+     </div>}
+     {result===false&&
+     <div className='result-false'>
+    Wrong!
+     </div>
+} 
+<div>Score: {score}</div>
     </div>
   );
 }
