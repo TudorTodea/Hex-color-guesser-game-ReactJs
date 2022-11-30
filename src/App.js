5077fa1906042b36b0095162b0d80f05b874ce81
TudorTodea <tudortodea1416@gmail.com>
2022-11-30 06:56:31 +0200

    changed the design

--- src/App.js
+++ src/App.js
@@ -1,66 +1,67 @@
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
 
-const[color,setColor]=useState('');
-const [answers,setAnswers]=useState([])
-const [result,setResult]=useState(null)
-const [score,setScore]=useState(0);
-const [bestScore,setBestScore]=useState(0)
-const generateColor=()=>{
-  const digits=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
-  
-  const color= new Array(6).fill(0).map((el)=> el=digits[Math.floor(Math.random()*digits.length)]).join('')
+  const [color, setColor] = useState('');
+  const [answers, setAnswers] = useState([])
+  const [result, setResult] = useState(null)
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0)
+  const generateColor = () => {
+    const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
 
-return '#'+color
-}
+    const color = new Array(6).fill(0).map((el) => el = digits[Math.floor(Math.random() * digits.length)]).join('')
 
-useEffect(()=>{
-const actualColor=generateColor();
-setColor(actualColor);
-setAnswers([actualColor,generateColor(),generateColor()].sort(()=>0.5 - Math.random()))
+    return '#' + color
+  }
 
-},[])
+  useEffect(() => {
+    const actualColor = generateColor();
+    setColor(actualColor);
+    setAnswers([actualColor, generateColor(), generateColor()].sort(() => 0.5 - Math.random()))
 
-const answerHandler=(e)=>{
-if(e.target.outerText===color){
-const actualColor=generateColor();
-setColor(actualColor);
-setAnswers([actualColor,generateColor(),generateColor()].sort(()=>0.5 - Math.random()))
-setResult(true)
-setScore((prev)=>{
-  if(prev+1>bestScore)
-  setBestScore(prev+1);
-return prev+1
-})
-}else{
-setResult(false)
-setScore(0);
-}
+  }, [])
 
-}
+  const answerHandler = (e) => {
+    if (e.target.outerText === color) {
+      const actualColor = generateColor();
+      setColor(actualColor);
+      setAnswers([actualColor, generateColor(), generateColor()].sort(() => 0.5 - Math.random()))
+      setResult(true)
+      setScore((prev) => {
+        if (prev + 1 > bestScore)
+          setBestScore(prev + 1);
+        return prev + 1
+      })
+    } else {
+      setResult(false)
+      setScore(0);
+    }
+
+  }
   return (
     <div className="App">
       <div className='container'>
-      <div>Best Score: {bestScore}</div>
-      <div style={{ backgroundColor:color}}  className='block'>
-      </div>
+
+        <div style={{ backgroundColor: color }} className='block'>
+          <div style={{ fontSize: '25px' }}>Best Score: {bestScore} <div>Score: {score}</div></div>
+          {result === true &&
+            <div className='result-corect'>
+              Corect!
+            </div>}
+          {result === false &&
+            <div className='result-false'>
+              Wrong!
+            </div>
+          }
+        </div>
       </div>
       <div className='answers-container'>
-       { answers.map((answer)=> <button onClick={(e)=>answerHandler(e)} className='answers' key={answer}>{answer}</button>)}
-          
+        {answers.map((answer) => <button onClick={(e) => answerHandler(e)} className='answers' key={answer}>{answer}</button>)}
+
       </div>
-      {result===true&&
-     <div className='result-corect'>
-    Corect!
-     </div>}
-     {result===false&&
-     <div className='result-false'>
-    Wrong!
-     </div>
-} 
-<div>Score: {score}</div>
+
     </div>
   );
 }
